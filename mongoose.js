dc3ca8f73844bf4969619c75420fff15742ada92
Andrew Cordiner <andrewcordiner@Andrews-MacBook-Pro.local>
2019-11-14 10:01:11 +1100

    Fixed deprecation warning

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/testing', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 module.exports = mongoose;
